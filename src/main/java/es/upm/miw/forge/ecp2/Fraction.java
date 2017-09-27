cc88ed9bee1812ec3a5be109797f57f3f98cc2c5
egraciani <egraciani@172.16.40.70>
2017-09-27 22:57:13 +0200

    Changing Fraction and test, to fix issue

--- src/main/java/es/upm/miw/forge/ecp2/Fraction.java
+++ src/main/java/es/upm/miw/forge/ecp2/Fraction.java
@@ -73,10 +73,10 @@ public class Fraction {
     	
     }
     
-    public double multiply(Fraction fractionToMultiply) {
-        double newNumerator = this.getNumerator() * fractionToMultiply.getNumerator()
-        double newDenominator = this.getDenominator() * fractionToMultiply.getDenominator()
-        double newFraction = newNumerator/newDenominator
+    public Fraction multiply(Fraction fractionToMultiply) {
+        int newNumerator = this.getNumerator() * fractionToMultiply.getNumerator();
+        int newDenominator = this.getDenominator() * fractionToMultiply.getDenominator();
+        newFraction = new Fraction (newNumerator, newDenominator);
         
         return newFraction;
     }
